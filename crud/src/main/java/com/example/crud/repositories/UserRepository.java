2157a1ad76c2927098791558e8c51ed6dfe52925
Karthikeyan Sivapragassam <karthikeyan.sivapragassam@nsearchglobal.com>
2023-03-28 17:17:21 +0800

    Adding Jenkinsfile

--- crud/src/main/java/com/example/crud/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.crud.repositories;
-
-import com.example.crud.model.User;
-import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Id> {
-}
