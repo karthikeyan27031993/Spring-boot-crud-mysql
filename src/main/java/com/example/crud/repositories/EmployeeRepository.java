a2069d0aa3acdf7d8372edfee1ffc8d51eb1d9aa
Karthikeyan Sivapragassam <karthikeyan.sivapragassam@nsearchglobal.com>
2023-03-30 14:28:00 +0800

    Learning crud operation in springboot

--- src/main/java/com/example/crud/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud.repositories;
-
-import com.example.crud.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
